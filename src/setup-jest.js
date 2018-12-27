567939f050359adb1ad9fe9403a77c6d7abae11a
Jerry Martins <zeroumbin@gmail.com>
2018-12-27 13:23:08 -0400

    estrutura de testes

---
+++ src/setup-jest.js
@@ -0,0 +1,11 @@
+import 'jest-preset-angular';
+import './jestGlobalMocks';
+
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
+    moduleNameMapper: {
+        '@core/(.*)': '<rootDir>/src/app/core/$1',
+        '@state/(.*)': '<rootDir>/src/app/state/$1'
+    }
+};
